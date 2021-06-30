f374dc13497122abf4f0f33670e3f6ffa51edd9e
Eudes Pavajeau <loleudes29@gmail.com>
2021-06-29 20:16:50 -0500

    Updates

---
+++ precache-manifest.62ce1dd0bc108845b5a4cb1921aee9e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5480d3cff91a2a403cc27eb58dcd3999",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "df6f5a62d5a8cbca6c4f",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "fee81b2c59e5924ff869",
+    "url": "/covid19-tracker/static/css/main.d7aee9af.chunk.css"
+  },
+  {
+    "revision": "df6f5a62d5a8cbca6c4f",
+    "url": "/covid19-tracker/static/js/2.c71aadd8.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid19-tracker/static/js/2.c71aadd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee81b2c59e5924ff869",
+    "url": "/covid19-tracker/static/js/main.737e9d48.chunk.js"
+  },
+  {
+    "revision": "099559483f4deef958d1",
+    "url": "/covid19-tracker/static/js/runtime-main.853463f0.js"
+  }
+]);
